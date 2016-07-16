b29d84dfa41eef8bf0dcf0f4b6470746baa2eab2
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2016-07-16 10:25:36 -0400

    added some more comments to the server.js file

--- server.js
+++ server.js
@@ -18,10 +18,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//session is used to keep the user logged in 
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}))
+
+//flash is used to show a message on an incorrect login
 app.use(flash());
+
+//passport middleware methods
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(express.static('public'));
 //-----------------------------------------------------------------
 
