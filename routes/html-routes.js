94b9367ddde84efa4f9caa3d3f57684bf956b0e4
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2016-07-16 10:22:54 -0400

    Added some comments to the routes file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,18 +3,26 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var orm = require('../db/orm.js');
 
+
+//Setting the strategy for Passport
 passport.use(new LocalStrategy({passReqToCallback : true},
   function(req, username, password, done) {
+
+  	//Searching the ORM for the user in the database
   	orm.findUser(username, function(err, user){
   		user = user[0];
   		if (err) { return done(err); }
       if (!user) { return done(null, false); }
+
+      //comparing user passwords - return if not a match
       if (password !== user.password) { return done(null, false);}
+
       return done(null, user);
   	});
   }
 ));
 
+//These two methods are required to keep the user logged in via the session
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
